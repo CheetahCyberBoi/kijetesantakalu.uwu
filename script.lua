3701c7eabf92744ce5a9d94e1b09718bb9df977d
BuildTools <unconfigured@null.spigotmc.org>
2024-06-11 18:34:04 -0700

    test

--- script.lua
+++ script.lua
@@ -3,5 +3,6 @@ local bottom_text = get("gaming")
 let funni_string = "kijetesantakalu"
 
 for i = 1,funni_string.len() do
+    print(funni_string[1]..funni_string[i])
     bottom_text.set_contents(funni_string[1]..funni_string[i])
 end
